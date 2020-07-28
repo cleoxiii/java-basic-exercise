18d563c0fc4f49f1e8ea0e041c109e9bae2ce3c4
Yun Chenxi <cleoxiii@163.com>
2020-07-29 00:01:27 +0800

    [yunchenxi] feat: should return common words with space.

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
@@ -15,16 +16,27 @@ public class GrammarExercise {
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         // please implement this method here
-        List<String> firstGroup = Arrays.asList(firstWordList.split(","));
-        List<String> secondGroup = Arrays.asList(secondWordList.split(","));
-        Predicate<String> isMatch = word -> word.matches("^[a-zA-Z]+$");
+        List<String> firstGroup = filterInput(firstWordList);
+        List<String> secondGroup = filterInput(secondWordList);
+        Predicate<String> isMatch = word -> word.matches("^[A-Z ]+$");
         if (!isInputValid(firstGroup, isMatch) || !isInputValid(secondGroup, isMatch)) {
             throw new RuntimeException("input not valid");
         }
-        return null;
+
+        List<String> result = firstGroup.stream()
+                                        .distinct().filter(secondGroup::contains)
+                                        .collect(Collectors.toList());
+        return result;
     }
 
     private static boolean isInputValid(List<String> list, Predicate predicate) {
         return list.stream().allMatch(predicate);
+    }
+
+    private static List<String> filterInput(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::toUpperCase)
+                .map(word -> word.replace(""," ").trim())
+                .collect(Collectors.toList());
     }
 }
